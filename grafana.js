29436b674e85f3f3ed7a4760b47682037b4ce430
Nayra1316 <RafaelDM@users.noreply.github.com>
2019-05-08 23:09:33 -0500

    Add files via upload

---
+++ grafana.js
@@ -0,0 +1,5 @@
+//yo no puedo entrar a grafana, pero si usted la corre sería con este comando
+docker run --name grafana -d -p 3000:3000 grafana/grafana
+//despues la abre en su navegador con este link y de esa manera puede ver la gráfica
+http://localhost:3000
+
