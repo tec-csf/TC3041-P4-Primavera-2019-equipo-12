c770df9e8ea7f414b3bfb7065cfdc4dac46f361d
Nayra1316 <RafaelDM@users.noreply.github.com>
2019-05-09 00:00:04 -0500

    Update grafana.js

--- grafana.js
+++ grafana.js
@@ -1,4 +1,4 @@
-//yo no puedo entrar a grafana, pero si usted la corre sería con este comando
+//Correr grafana
 docker run --name grafana -d -p 3000:3000 grafana/grafana
 //despues la abre en su navegador con este link y de esa manera puede ver la gráfica
 http://localhost:3000
